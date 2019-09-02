86020646e9abbe7b734de73d7d0097553f611e69
ANonymous <postmaster.richmama@gmail.com>
2019-09-02 17:33:52 +0100

    facelift

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -36,15 +36,16 @@
       <div class="row mt-5">
 
         <div class="col-12 mb-5 order-2">
-          <form action="#" method="post">
+          <form action="{{url('contact')}}" method="post">
+			  {{csrf_field()}}
             <div class="row">
               <div class="col-md-6 form-group">
                 <label for="name">Name</label>
-                <input type="text" id="name" class="form-control ">
+                <input type="text" name="name" id="name"  value="{{old('name')}}" class="form-control ">
               </div>
               <div class="col-md-6 form-group">
                 <label for="phone">Phone</label>
-                <input type="text" id="phone" class="form-control ">
+                <input type="text" id="phone" name="phone" value="{{old('phone')}}" class="form-control ">
               </div>
             </div>
             <div class="row">
@@ -55,13 +56,13 @@
             <div class="row">
               <div class="col-md-12 form-group">
                 <label for="email">Email</label>
-                <input type="email" id="email" class="form-control ">
+                <input type="email" id="email" value="{{old('email')}}" name="email" class="form-control ">
               </div>
             </div>
             <div class="row">
               <div class="col-md-12 form-group">
                 <label for="message">Write Message</label>
-                <textarea name="message" id="message" class="form-control " cols="30" rows="8"></textarea>
+                <textarea name="message" value="{{old('message')}}" id="message" class="form-control " cols="30" rows="8"></textarea>
               </div>
             </div>
             <div class="row">
